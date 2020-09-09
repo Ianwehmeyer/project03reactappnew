fc1296e67aad8da160a055fb23112790b84c88e7
Ianwehmeyer <65263828+Ianwehmeyer@users.noreply.github.com>
2020-09-08 22:06:05 -0400

    Nav updates
    will be building CSS to make better + may use hamburger

--- src/Nav.js
+++ src/Nav.js
@@ -4,14 +4,18 @@ import {Link} from 'react-router-dom'
 
 export default function Nav() {
     return (
-        <nav>
-            <ul>
+        
+        <nav className="Navbar" >
+            <div>Possible Hamburger button - do later</div>
+            <div>
+                <ul>
                 
-                <li><Link to="/">To Do List</Link></li>
-                <li><Link to="/About">About</Link></li>
-                <li><Link to="/Contact">Contact</Link></li>
+                    <li><Link to="/">To Do List</Link></li>
+                    <li><Link to="/About">About</Link></li>
+                    <li><Link to="/Contact">Contact</Link></li>
                 
-            </ul>
+                </ul>
+            </div>
         </nav>
     )
 }
